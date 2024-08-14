2729466057f4da137bd2cd68475976d349420b42
mascarenhas2 <160731737+mascarenhas2@users.noreply.github.com>
2024-08-14 00:33:52 +0000

    sdd

--- estudos/src/main/java/com/example/Main.java
+++ estudos/src/main/java/com/example/Main.java
@@ -2,8 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente clientes = new Cliente("23344", "Caio", "71991315143", "85775229541",Sexo.MASCULINO, Endereco);
         Endereco endereco = new Endereco("Rua Souto Dalva", 78, "40301065", "Bahia", "Salvador");
+        Cliente clientes = new Cliente("23344", "Caio", "71991315143", "85775229541",Sexo.MASCULINO, endereco);
     
         System.out.println("Id: "+ clientes.getId());
         System.out.println("Nome: "+ clientes.getNome());
