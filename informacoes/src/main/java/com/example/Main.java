7dd44e5effc6ea23684fd0e997eed932598c12cb
mascarenhas2 <160731737+mascarenhas2@users.noreply.github.com>
2024-08-19 14:29:19 +0000

    simsim

--- informacoes/src/main/java/com/example/Main.java
+++ informacoes/src/main/java/com/example/Main.java
@@ -3,8 +3,6 @@ package com.example;
 public abstract class Main {
     public static void main(String[] args) {
        
-       Fisica fisica = new 
-       Juridica juridica = new Fisica(0, null, null, new Endereco(0, null, null, null, null, null, null, null, null, null), null, null, null, null, 0)
-
-    }
+       Fisica fisica = new Fisica
+       Juridica juridica = new Juridica
 }
